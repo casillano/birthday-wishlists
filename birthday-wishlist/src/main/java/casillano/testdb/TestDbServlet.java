dceeba552515123cb0c5ce78177bc709f13db572
Dominic <dom.casillano@mail.utoronto.ca>
2020-06-13 21:27:28 -0400

    Initial commit. Successful connection to local database

---
+++ birthday-wishlist/src/main/java/casillano/testdb/TestDbServlet.java
@@ -0,0 +1,42 @@
+package casillano.testdb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.DriverManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+
+@WebServlet("/servlet/casillano.testdb.TestDbServlet")
+public class TestDbServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String user="springstudent";
+		String pass="springstudent";
+		
+		String url="jdbc:mysql://localhost:3306/birthday-wishlist-db?useSSL=false&serverTimezone=UTC";
+		String driver = "com.mysql.cj.jdbc.Driver";
+		
+		try {
+			PrintWriter out = response.getWriter();
+			out.println("Connection to databse: " + url);
+			Class.forName(driver);
+			Connection conn = DriverManager.getConnection(url, user, pass);
+			out.println("Connection successful");
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ServletException(e);
+		}
+		
+	}
+	
+
+}
